bf982107fd043a538fcdf24cf9aeba96b14c750a
Katherine Byunn-Rieder <kbyunnrieder@college.harvard.edu>
2023-10-21 13:57:08 -0400

    gets url from active tab

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.action.onClicked.addListener((tab) => {
+  const taburl = tab.url;
+  const youurl = "https://you.com/search?q=summarize+" + taburl;
+  getHtml(youurl);
+});
+
+func getHtml(url){
+  
+}
