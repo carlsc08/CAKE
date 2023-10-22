9e7e12f9f81837473433b04c4e06e341f38c23fc
Katherine Byunn-Rieder <kbyunnrieder@college.harvard.edu>
2023-10-21 23:45:43 -0400

    pseudocode for integration of all parts of program

--- script.js
+++ script.js
@@ -15,8 +15,38 @@ function getCurSelection(){
  */
 function main(selection){
     if (selection){
-        alert(selection);
+        //query
+
+        //display query result
+        const str = "weeee"; //CHANGE TO EQUAL QUERY RESULT
+        const map = stringsToMap(str);
+        displayKeywords(map);
+
+        //open popup
+        window.open({
+            type: 'popup',
+            url: 'hello.html',
+            width: 400,
+            height: 600
+        });
     } else {
         alert("i am sad! i found no selection :(");
+    }
+}
+
+function stringToMap(str) {
+    let stringsMap = new Map();
+    let lines = str.split(/\r?\n/);
+    for (let i = 0; i < 2*(lines.length-1); i+=2) {
+        let substrings = str.split('**');
+        substrings.shift();
+        stringsMap.set(substrings[i], substrings[i+1]);
+    }
+    return (stringsMap);
+}
+
+function displayKeywords(map){
+    for (let [key, value] in map){
+        //document.write("<p></p>");
     }
 }
