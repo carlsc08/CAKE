877cdcbf55674479e63d3d63d20541244e1d45b9
Katherine Byunn-Rieder <kbyunnrieder@college.harvard.edu>
2023-10-21 22:21:18 -0400

    collects selection from user

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-document.getElementById("run").onclick = getCurrentTabURL();
+getCurSelection();
 
-async function getCurrentTabURL() {
-    let queryOptions = {active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    if (tab == 'undefined'){
-        throw "no current tab";
+function getCurSelection(){
+    var text = "";
+    if (window.getSelection()) {
+        text = window.getSelection().toString();
+    } else {
+        text = "im sad. i couldnt find a selection! please make one!";
     }
-    let youurl = "https://you.com/search?q=summarize+" + tab.url;
-    console.log("here: " + youurl);
+    main(text);
 }
 
-function getHtml(url){
-  
+/**
+ * @param {selection} holds what user had highlighted when they clicked on the extension
+ */
+function main(selection){
+    if (selection){
+        alert(selection);
+    } else {
+        alert("i am sad! i found no selection :(");
+    }
 }
-
-/*
- "content_scripts": [{
-    "matches": ["<all_urls>"],
-        "all_frames": true,
-        "js": ["content_scripts.js"]
-  }],
-  */
